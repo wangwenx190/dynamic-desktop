f3b5a3657da33e3b5910772cbbe48b7da090094f
wangwenx190 <2546789017@qq.com>
2018-09-24 14:41:05 +0800

    Update.

--- translations/dd_en.ts
+++ translations/dd_en.ts
@@ -5,107 +5,118 @@
     <name>PreferencesDialog</name>
     <message>
         <location filename="../forms/preferencesdialog.ui" line="20"/>
+        <location filename="../forms/preferencesdialog.ui" line="220"/>
         <source>Preferences</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../forms/preferencesdialog.ui" line="26"/>
+        <location filename="../forms/preferencesdialog.ui" line="306"/>
         <source>General</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../forms/preferencesdialog.ui" line="32"/>
+        <location filename="../forms/preferencesdialog.ui" line="312"/>
         <source>Auto start</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../forms/preferencesdialog.ui" line="42"/>
+        <location filename="../forms/preferencesdialog.ui" line="319"/>
+        <source>Localize</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../forms/preferencesdialog.ui" line="329"/>
         <source>Player</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../forms/preferencesdialog.ui" line="50"/>
+        <location filename="../forms/preferencesdialog.ui" line="337"/>
         <source>Try to use Hardware Accelerated Decoding</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../forms/preferencesdialog.ui" line="70"/>
+        <location filename="../forms/preferencesdialog.ui" line="357"/>
         <source>CUDA</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../forms/preferencesdialog.ui" line="77"/>
+        <location filename="../forms/preferencesdialog.ui" line="364"/>
         <source>D3D11</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../forms/preferencesdialog.ui" line="84"/>
+        <location filename="../forms/preferencesdialog.ui" line="371"/>
         <source>DXVA</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../forms/preferencesdialog.ui" line="95"/>
-        <location filename="../forms/preferencesdialog.cpp" line="51"/>
+        <location filename="../forms/preferencesdialog.ui" line="382"/>
+        <location filename="../forms/preferencesdialog.cpp" line="72"/>
         <source>URL</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../forms/preferencesdialog.ui" line="108"/>
+        <location filename="../forms/preferencesdialog.ui" line="395"/>
         <source>Local files and web urls are all acceptable</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../forms/preferencesdialog.ui" line="115"/>
+        <location filename="../forms/preferencesdialog.ui" line="402"/>
         <source>Input</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../forms/preferencesdialog.ui" line="122"/>
+        <location filename="../forms/preferencesdialog.ui" line="409"/>
         <source>Browse</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../forms/preferencesdialog.ui" line="133"/>
+        <location filename="../forms/preferencesdialog.ui" line="420"/>
         <source>Volume</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../forms/preferencesdialog.ui" line="151"/>
+        <location filename="../forms/preferencesdialog.ui" line="438"/>
         <source>Play</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../forms/preferencesdialog.ui" line="158"/>
+        <location filename="../forms/preferencesdialog.ui" line="445"/>
         <source>Pause</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../forms/preferencesdialog.ui" line="185"/>
+        <location filename="../forms/preferencesdialog.ui" line="472"/>
         <source>OK</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../forms/preferencesdialog.ui" line="192"/>
+        <location filename="../forms/preferencesdialog.ui" line="479"/>
         <source>Cancel</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../forms/preferencesdialog.cpp" line="43"/>
+        <location filename="../forms/preferencesdialog.cpp" line="26"/>
+        <source>Application restart is needed to apply this change.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../forms/preferencesdialog.cpp" line="64"/>
         <source>Please select a media file</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../forms/preferencesdialog.cpp" line="43"/>
+        <location filename="../forms/preferencesdialog.cpp" line="64"/>
         <source>Videos (*.avi *.mp4 *.mkv *.flv);;All files (*)</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../forms/preferencesdialog.cpp" line="51"/>
+        <location filename="../forms/preferencesdialog.cpp" line="72"/>
         <source>Please input a valid url</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../forms/preferencesdialog.cpp" line="81"/>
+        <location filename="../forms/preferencesdialog.cpp" line="102"/>
         <source>Restart this application to experience it.
 Make sure this application runs in your GPU&apos;s Optimus mode.</source>
         <translation type="unfinished"></translation>
@@ -114,37 +125,37 @@ Make sure this application runs in your GPU&apos;s Optimus mode.</source>
 <context>
     <name>QObject</name>
     <message>
-        <location filename="../main.cpp" line="66"/>
+        <location filename="../main.cpp" line="69"/>
         <source>There is another instance running. Please do not run twice.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="77"/>
+        <location filename="../main.cpp" line="80"/>
         <source>My wallpaper</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="105"/>
+        <location filename="../main.cpp" line="108"/>
         <source>Preferences</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="120"/>
+        <location filename="../main.cpp" line="123"/>
         <source>Play</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="121"/>
+        <location filename="../main.cpp" line="131"/>
         <source>Pause</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="122"/>
+        <location filename="../main.cpp" line="132"/>
         <source>Mute</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="136"/>
+        <location filename="../main.cpp" line="146"/>
         <source>Exit</source>
         <translation type="unfinished"></translation>
     </message>
--- translations/dd_zh_CN.ts
+++ translations/dd_zh_CN.ts
@@ -5,107 +5,118 @@
     <name>PreferencesDialog</name>
     <message>
         <location filename="../forms/preferencesdialog.ui" line="20"/>
+        <location filename="../forms/preferencesdialog.ui" line="220"/>
         <source>Preferences</source>
         <translation>首选项</translation>
     </message>
     <message>
-        <location filename="../forms/preferencesdialog.ui" line="26"/>
+        <location filename="../forms/preferencesdialog.ui" line="306"/>
         <source>General</source>
         <translation>通用</translation>
     </message>
     <message>
-        <location filename="../forms/preferencesdialog.ui" line="32"/>
+        <location filename="../forms/preferencesdialog.ui" line="312"/>
         <source>Auto start</source>
         <translation>开机自启</translation>
     </message>
     <message>
-        <location filename="../forms/preferencesdialog.ui" line="42"/>
+        <location filename="../forms/preferencesdialog.ui" line="319"/>
+        <source>Localize</source>
+        <translation>本地化</translation>
+    </message>
+    <message>
+        <location filename="../forms/preferencesdialog.ui" line="329"/>
         <source>Player</source>
         <translation>播放器</translation>
     </message>
     <message>
-        <location filename="../forms/preferencesdialog.ui" line="50"/>
+        <location filename="../forms/preferencesdialog.ui" line="337"/>
         <source>Try to use Hardware Accelerated Decoding</source>
         <translation>尝试使用硬件加速解码</translation>
     </message>
     <message>
-        <location filename="../forms/preferencesdialog.ui" line="70"/>
+        <location filename="../forms/preferencesdialog.ui" line="357"/>
         <source>CUDA</source>
         <translation>CUDA</translation>
     </message>
     <message>
-        <location filename="../forms/preferencesdialog.ui" line="77"/>
+        <location filename="../forms/preferencesdialog.ui" line="364"/>
         <source>D3D11</source>
         <translation>D3D11</translation>
     </message>
     <message>
-        <location filename="../forms/preferencesdialog.ui" line="84"/>
+        <location filename="../forms/preferencesdialog.ui" line="371"/>
         <source>DXVA</source>
         <translation>DXVA</translation>
     </message>
     <message>
-        <location filename="../forms/preferencesdialog.ui" line="95"/>
-        <location filename="../forms/preferencesdialog.cpp" line="51"/>
+        <location filename="../forms/preferencesdialog.ui" line="382"/>
+        <location filename="../forms/preferencesdialog.cpp" line="72"/>
         <source>URL</source>
         <translation>路径</translation>
     </message>
     <message>
-        <location filename="../forms/preferencesdialog.ui" line="108"/>
+        <location filename="../forms/preferencesdialog.ui" line="395"/>
         <source>Local files and web urls are all acceptable</source>
         <translation>可接受本地文件的路径或任何有效的网址</translation>
     </message>
     <message>
-        <location filename="../forms/preferencesdialog.ui" line="115"/>
+        <location filename="../forms/preferencesdialog.ui" line="402"/>
         <source>Input</source>
         <translation>输入</translation>
     </message>
     <message>
-        <location filename="../forms/preferencesdialog.ui" line="122"/>
+        <location filename="../forms/preferencesdialog.ui" line="409"/>
         <source>Browse</source>
         <translation>浏览</translation>
     </message>
     <message>
-        <location filename="../forms/preferencesdialog.ui" line="133"/>
+        <location filename="../forms/preferencesdialog.ui" line="420"/>
         <source>Volume</source>
         <translation>音量</translation>
     </message>
     <message>
-        <location filename="../forms/preferencesdialog.ui" line="151"/>
+        <location filename="../forms/preferencesdialog.ui" line="438"/>
         <source>Play</source>
         <translation>播放</translation>
     </message>
     <message>
-        <location filename="../forms/preferencesdialog.ui" line="158"/>
+        <location filename="../forms/preferencesdialog.ui" line="445"/>
         <source>Pause</source>
         <translation>暂停</translation>
     </message>
     <message>
-        <location filename="../forms/preferencesdialog.ui" line="185"/>
+        <location filename="../forms/preferencesdialog.ui" line="472"/>
         <source>OK</source>
         <translation>确定</translation>
     </message>
     <message>
-        <location filename="../forms/preferencesdialog.ui" line="192"/>
+        <location filename="../forms/preferencesdialog.ui" line="479"/>
         <source>Cancel</source>
         <translation>取消</translation>
     </message>
     <message>
-        <location filename="../forms/preferencesdialog.cpp" line="43"/>
+        <location filename="../forms/preferencesdialog.cpp" line="26"/>
+        <source>Application restart is needed to apply this change.</source>
+        <translation>重启此应用程序以生效。</translation>
+    </message>
+    <message>
+        <location filename="../forms/preferencesdialog.cpp" line="64"/>
         <source>Please select a media file</source>
         <translation>请选择一个媒体文件</translation>
     </message>
     <message>
-        <location filename="../forms/preferencesdialog.cpp" line="43"/>
+        <location filename="../forms/preferencesdialog.cpp" line="64"/>
         <source>Videos (*.avi *.mp4 *.mkv *.flv);;All files (*)</source>
         <translation>视频 (*.avi *.mp4 *.mkv *.flv);;所有文件 (*)</translation>
     </message>
     <message>
-        <location filename="../forms/preferencesdialog.cpp" line="51"/>
+        <location filename="../forms/preferencesdialog.cpp" line="72"/>
         <source>Please input a valid url</source>
         <translation>请输入一个有效的网址</translation>
     </message>
     <message>
-        <location filename="../forms/preferencesdialog.cpp" line="81"/>
+        <location filename="../forms/preferencesdialog.cpp" line="102"/>
         <source>Restart this application to experience it.
 Make sure this application runs in your GPU&apos;s Optimus mode.</source>
         <translation>重新运行此程序以体验改变。
@@ -119,37 +130,37 @@ Make sure this application runs in your GPU&apos;s Optimus mode.</source>
         <translation type="vanished">另一个实例已经在运行。请勿重复运行。</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="66"/>
+        <location filename="../main.cpp" line="69"/>
         <source>There is another instance running. Please do not run twice.</source>
         <translation>另一个实例已经在运行。请勿重复运行。</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="77"/>
+        <location filename="../main.cpp" line="80"/>
         <source>My wallpaper</source>
         <translation>我的壁纸</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="105"/>
+        <location filename="../main.cpp" line="108"/>
         <source>Preferences</source>
         <translation>首选项</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="120"/>
+        <location filename="../main.cpp" line="123"/>
         <source>Play</source>
         <translation>播放</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="121"/>
+        <location filename="../main.cpp" line="131"/>
         <source>Pause</source>
         <translation>暂停</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="122"/>
+        <location filename="../main.cpp" line="132"/>
         <source>Mute</source>
         <translation>静音</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="136"/>
+        <location filename="../main.cpp" line="146"/>
         <source>Exit</source>
         <translation>退出</translation>
     </message>
