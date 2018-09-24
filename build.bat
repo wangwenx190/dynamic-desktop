85cd0c2e7da9bdda9f65737541f0998e5db93380
wangwenx190 <2546789017@qq.com>
2018-09-24 15:42:20 +0800

    Update.

---
+++ build.bat
@@ -0,0 +1,17 @@
+if exist "%~dp0build" rd /s /q "%~dp0build"
+lupdate dd.pro
+lrelease dd.pro
+qmake dd.pro -spec win32-msvc CONFIG+=release
+jom qmake_all
+jom && jom install
+if exist "%~dp0build\bin" (
+    cd /d "%~dp0build\bin"
+    if exist plugins rd /s /q plugins
+    if exist translations rd /s /q translations
+    windeployqt --plugindir "%~dp0build\bin\plugins" --release --force --no-translations --compiler-runtime --angle --no-opengl-sw dd.exe
+) else (
+    cd /d "%~dp0build\bin64"
+    if exist plugins rd /s /q plugins
+    if exist translations rd /s /q translations
+    windeployqt --plugindir "%~dp0build\bin64\plugins" --release --force --no-translations --compiler-runtime --angle --no-opengl-sw dd64.exe
+)
