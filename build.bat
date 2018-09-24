d08580f094bdeb289ea066cf7811c23ad5a1067f
wangwenx190 <2546789017@qq.com>
2018-09-24 15:52:08 +0800

    Update.

--- build.bat
+++ build.bat
@@ -7,11 +7,10 @@ jom && jom install
 if exist "%~dp0build\bin" (
     cd /d "%~dp0build\bin"
     if exist plugins rd /s /q plugins
-    if exist translations rd /s /q translations
     windeployqt --plugindir "%~dp0build\bin\plugins" --release --force --no-translations --compiler-runtime --angle --no-opengl-sw dd.exe
 ) else (
     cd /d "%~dp0build\bin64"
     if exist plugins rd /s /q plugins
-    if exist translations rd /s /q translations
     windeployqt --plugindir "%~dp0build\bin64\plugins" --release --force --no-translations --compiler-runtime --angle --no-opengl-sw dd64.exe
 )
+cd /d "%~dp0"
