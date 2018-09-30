2b9851c5e5f61c4259fabe5a3444fa20e6ce2d89
wangwenx190 <2546789017@qq.com>
2018-09-30 12:50:42 +0800

    Update.

--- build.bat
+++ build.bat
@@ -1,11 +1,26 @@
+:: usage: call build.bat "mkspec" "config"
+:: eg: call build.bat "win32-icc" "release static_build"
+:: IMPORTANT: Double quotation marks are indispensable
 cd /d "%~dp0"
 if exist build rd /s /q build
 md build
 cd build
 set _mkspec=%1
+if defined _mkspec (
+    set _mkspec=%_mkspec:~1,-1%
+) else (
+    where clang-cl
+    if %ERRORLEVEL% == 0 set _mkspec=win32-clang-msvc
+    where icl
+    if %ERRORLEVEL% == 0 set _mkspec=win32-icc
+)
 if /i "%_mkspec%" == "" set _mkspec=win32-msvc
 set _config=%2
-if /i "%_config%" == "" set _config=release
+if defined _config (
+    set _config=%_config:~1,-1%
+) else (
+    set _config=release
+)
 qmake "%~dp0dynamic-desktop.pro" -spec %_mkspec% "CONFIG+=%_config%"
 set _buildtool=jom
 where %_buildtool%
--- ci/install.bat
+++ ci/install.bat
@@ -1,21 +1,22 @@
-if exist depends rd /s /q depends
+if exist qtavdepends.7z del /f qtavdepends.7z
 appveyor DownloadFile "https://sourceforge.net/projects/qtav/files/depends/QtAV-depends-windows-x86+x64.7z/download" -FileName qtavdepends.7z
-7z x qtavdepends.7z
+if exist depends rd /s /q depends
+7z x qtavdepends.7z > NUL
 del /f qtavdepends.7z
 move QtAV-depends* depends
-xcopy /s /y /i depends\include %QTDIR%\include
-xcopy /s /y /i depends\lib\x64 %QTDIR%\lib
+xcopy /s /y /i depends\include %QTDIR%\include > NUL
+xcopy /s /y /i depends\lib\x64 %QTDIR%\lib > NUL
 rd /s /q depends
 if exist av rd /s /q av
 md av
 cd av
 appveyor DownloadFile "https://sourceforge.net/projects/qtav/files/ci/QtAV-Qt5.9-VS2017x64-release-%QTAVID%.7z/download" -FileName qtavci.7z
-7z x qtavci.7z
-xcopy /s /y /i include %QTDIR%\include
-copy /y lib\*Qt*AV*.lib %QTDIR%\lib
-copy /y lib\QtAV1.lib %QTDIR%\lib\Qt5AV.lib
-copy /y lib\QtAVWidgets1.lib %QTDIR%\lib\Qt5AVWidgets.lib
-xcopy /s /y /i bin\QtAV %QTDIR%\qml\QtAV
-xcopy /s /y /i mkspecs %QTDIR%\mkspecs
+7z x qtavci.7z > NUL
+xcopy /s /y /i include %QTDIR%\include > NUL
+copy /y lib\*Qt*AV*.lib %QTDIR%\lib > NUL
+copy /y lib\QtAV1.lib %QTDIR%\lib\Qt5AV.lib > NUL
+copy /y lib\QtAVWidgets1.lib %QTDIR%\lib\Qt5AVWidgets.lib > NUL
+xcopy /s /y /i bin\QtAV %QTDIR%\qml\QtAV > NUL
+xcopy /s /y /i mkspecs %QTDIR%\mkspecs > NUL
 cd ..
 rd /s /q av
