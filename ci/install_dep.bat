a9c999144b0d863c0311408f849a1c37706a3fd4
wangwenx190 <2546789017@qq.com>
2018-10-26 00:43:22 +0800

    Removed useless code.

--- ci/install_dep.bat
+++ ci/install_dep.bat
@@ -7,7 +7,7 @@ del /f qtavdepends.7z
 move QtAV-depends* depends
 xcopy /s /y /i depends\include %QTDIR%\include > NUL
 xcopy /s /y /i depends\lib\x64 %QTDIR%\lib > NUL
-copy /y depends\bin\x64\OpenAL32-VS2015.dll %QTDIR%\bin\OpenAL32.dll > NUL
+::copy /y depends\bin\x64\OpenAL32-VS2015.dll %QTDIR%\bin\OpenAL32.dll > NUL
 rd /s /q depends
 if exist av rd /s /q av
 md av
@@ -20,22 +20,22 @@ copy /y lib\QtAV1.lib %QTDIR%\lib\Qt5AV.lib > NUL
 copy /y lib\QtAVWidgets1.lib %QTDIR%\lib\Qt5AVWidgets.lib > NUL
 xcopy /s /y /i bin\QtAV %QTDIR%\qml\QtAV > NUL
 xcopy /s /y /i mkspecs %QTDIR%\mkspecs > NUL
-copy /y bin\*Qt*AV*.dll %QTDIR%\bin > NUL
+::copy /y bin\*Qt*AV*.dll %QTDIR%\bin > NUL
 cd ..
 rd /s /q av
-if exist ffmpeg rd /s /q ffmpeg
-md ffmpeg
-cd ffmpeg
-appveyor DownloadFile "https://sourceforge.net/projects/avbuild/files/windows-desktop/ffmpeg-git-desktop-VS2017-lite.7z/download" -FileName qtavffmpeg.7z
-7z x qtavffmpeg.7z > NUL
-copy /y ffmpeg-git-desktop-VS2017-lite\bin\x64\*.dll %QTDIR%\bin > NUL
-cd ..
-rd /s /q ffmpeg
-if exist libass rd /s /q libass
-md libass
-cd libass
-appveyor DownloadFile "https://github.com/ShiftMediaProject/libass/releases/download/%SMP_LIBASS_VERSION%/libass_%SMP_LIBASS_VERSION%_msvc15.zip" -FileName smplibass.zip
-7z x smplibass.zip > NUL
-copy /y bin\x64\ass.dll %QTDIR%\bin\libass.dll > NUL
-cd ..
-rd /s /q libass
+::if exist ffmpeg rd /s /q ffmpeg
+::md ffmpeg
+::cd ffmpeg
+::appveyor DownloadFile "https://sourceforge.net/projects/avbuild/files/windows-desktop/ffmpeg-git-desktop-VS2017-lite.7z/download" -FileName qtavffmpeg.7z
+::7z x qtavffmpeg.7z > NUL
+::copy /y ffmpeg-git-desktop-VS2017-lite\bin\x64\*.dll %QTDIR%\bin > NUL
+::cd ..
+::rd /s /q ffmpeg
+::if exist libass rd /s /q libass
+::md libass
+::cd libass
+::appveyor DownloadFile "https://github.com/ShiftMediaProject/libass/releases/download/%SMP_LIBASS_VERSION%/libass_%SMP_LIBASS_VERSION%_msvc15.zip" -FileName smplibass.zip
+::7z x smplibass.zip > NUL
+::copy /y bin\x64\ass.dll %QTDIR%\bin\libass.dll > NUL
+::cd ..
+::rd /s /q libass
