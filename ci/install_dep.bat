4e0ea7fa843c34155618c80bc571ea9516248a07
wangwenx190 <2546789017@qq.com>
2018-11-16 16:57:03 +0800

    Update.

--- ci/install_dep.bat
+++ ci/install_dep.bat
@@ -1,11 +1,11 @@
 @echo off
-if exist ffmpeg rd /s /q ffmpeg
-md ffmpeg
-cd ffmpeg
-appveyor DownloadFile "https://sourceforge.net/projects/avbuild/files/windows-desktop/ffmpeg-git-desktop-VS2017-lite.7z/download" -FileName qtavffmpeg.7z
-7z x qtavffmpeg.7z > NUL
-del /f qtavffmpeg.7z
+if exist qtavffmpeg rd /s /q qtavffmpeg
+md qtavffmpeg
+cd qtavffmpeg
+appveyor DownloadFile "https://sourceforge.net/projects/avbuild/files/windows-desktop/ffmpeg-git-desktop-VS2017-lite.7z/download" -FileName avbuild.7z
+7z x avbuild.7z > NUL
+del /f avbuild.7z
 move ffmpeg-* ffmpeg
 xcopy /s /y /i ffmpeg %APPVEYOR_BUILD_FOLDER%\ffmpeg > NUL
 cd ..
-rd /s /q ffmpeg
+rd /s /q qtavffmpeg
