c74e88e1b1c971340ddc6e74664d76577fe7bfb0
wangwenx190 <2546789017@qq.com>
2018-11-24 23:21:06 +0800

    Update.

--- ci/update_translations.bat
+++ ci/update_translations.bat
@@ -1,8 +1,2 @@
 @echo off
-if exist build rd /s /q build
-md build
-cd build
 lupdate -no-obsolete -locations none -no-ui-lines "%~dp0..\src\ddmain\ddmain.pro"
-lrelease -nounfinished -removeidentical "%~dp0..\src\ddmain\ddmain.pro"
-cd ..
-rd /s /q build
