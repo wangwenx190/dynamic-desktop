f30957bb9827619a6b3719e672f56c42cfffb1f8
wangwenx190 <2546789017@qq.com>
2018-11-28 21:51:37 +0800

    Update.

--- src/ddmain/forms/aboutdialog.cpp
+++ src/ddmain/forms/aboutdialog.cpp
@@ -13,15 +13,24 @@ AboutDialog::AboutDialog(QWidget *parent) : QWidget(parent)
     ui->lineEdit_commit_time->setText(QStringLiteral(DD_COMMIT_TIME));
     QString compiler = QStringLiteral("Unknown");
 #ifdef Q_CC_CLANG
-    compiler = QStringLiteral("Clang C++ Compiler v%0.%1.%2").arg(__clang_major__).arg(__clang_minor__).arg(__clang_patchlevel__);
+    compiler = QStringLiteral("Clang C++ Compiler V%0.%1.%2").arg(__clang_major__).arg(__clang_minor__).arg(__clang_patchlevel__);
 #elif defined(Q_CC_MSVC)
-    compiler = QStringLiteral("Microsoft Visual C++ Compiler v%0").arg(_MSC_FULL_VER);
+    QString msvcFullVer = QString::number(_MSC_FULL_VER);
+    msvcFullVer.insert(2, QLatin1Char('.'));
+    msvcFullVer.insert(5, QLatin1Char('.'));
+    msvcFullVer.append(QLatin1Char('.'));
+    QString msvcBuildVer = QString::number(_MSC_BUILD);
+    if (_MSC_BUILD < 10)
+        msvcBuildVer.prepend(QLatin1Char('0'));
+    msvcFullVer.append(msvcBuildVer);
+    compiler = QStringLiteral("Microsoft Visual C++ Compiler V") + msvcFullVer;
 #elif defined(Q_CC_INTEL)
-    compiler = QStringLiteral("Intel C++ Compiler v%0").arg(_MSC_FULL_VER); // How to detect ICC version?
+    compiler = QStringLiteral("Intel C++ Compiler V%0").arg(_MSC_VER); // How to detect ICC version?
 #elif defined(Q_CC_GNU)
-    compiler = QStringLiteral("GNU C++ Compiler v%0.%1.%2").arg(__GNUC__).arg(__GNUC_MINOR__).arg(__GNUC_PATCHLEVEL__); // Are these macros correct?
+    compiler = QStringLiteral("GNU C++ Compiler V%0.%1.%2").arg(__GNUC__).arg(__GNUC_MINOR__).arg(__GNUC_PATCHLEVEL__); // Are these macros correct?
 #endif
     ui->lineEdit_compiler->setText(compiler);
+    ui->lineEdit_compiler->setCursorPosition(0);
 #ifdef WIN64
     const QString arch = QStringLiteral("x64");
 #else
